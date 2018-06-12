77e75e3664f3cf6924a449ade9f04094c6d04633
ChungTV <tranchung927@gmail.com>
2018-06-12 09:43:13 +0700

    T02.01-Solution-CreateLayout

---
+++ Lesson02GitHubRepoSearch/app/src/main/java/com/big0/chung/lesson02_github_repo_search/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.big0.chung.lesson02_github_repo_search
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
